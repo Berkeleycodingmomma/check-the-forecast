038abbb1721bd1d1bbe1f5c388eb5445af6b8fbc
Amanda Gray <floridabelle2021@protonmail.com>
2023-04-08 15:48:04 -0600

    added the getElements and event listners

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+// #string-lib 
+
+// JS library for Strings  
+
+console.log("hello world")
+
+
+function allLetter(inputtxt)
+      { 
+      var letters = /^[A-Za-z]+$/;
+      if(inputtxt.value.match(letters))
+      {
+      alert('Your name have accepted : you can try another');
+      return true;
+      }
+      else
+      {
+      alert('Please input alphabet characters only');
+      return false;
+      }
+      }
