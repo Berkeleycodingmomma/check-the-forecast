ec01c5c6f52306533e1daaaa83189a780345efd1
Amanda Gray <floridabelle2021@protonmail.com>
2023-04-08 18:17:05 -0600

    created buttons from search histroy

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,16 +9,40 @@
  // bellow is going to save the search history
  var cityHist = [];
  $('.searc').on("click", function (event) {
-             event.preventDefault();
-             city = $(this).parent(".btnPar").siblings(".textVal").val().trim();
-                 if (city === "") {
-                     return;
-                 }; cityHist.push(city);
+     event.preventDefault();
+     city = $(this).parent(".btnPar").siblings(".textVal").val().trim();
+     if (city === "") {
+         return;
+     };
+     cityHist.push(city);
 
-                 localStorage.setItem('citu', JSON.stringify(cityHist));
-                 fiveForecastEl.empy(); getHistory(); getWeatherToday();
+     localStorage.setItem('citu', JSON.stringify(cityHist));
+     fiveForecastEl.empy();
+     getHistory();
+     getWeatherToday();
 
-             });
+ });
+// below are buttons from the search history
+ var contHistEl = $(".cityHistory");
+
+ function getHistory() {
+     contHistEl.empty();
+
+     for (let i = 0; i < cityHist.length; i++) {
+         var rowEl = $('<row>');
+         var btnEl = $('<button>').text('${cityHist[i]')
+
+         rowEl.addClass('row histBtnRow');
+         btnEl.addClass('btn btn-outline-secondary histBtn');
+
+         contHistEl.prepend(rowEl);
+         rowEl.append(btnEl);
+     }
+
+     if (!city){
+        return;
+     }
+ }
 
 
 
@@ -29,6 +53,4 @@
 
 
 
-
-
-         // 89c4250809c202e3e1990eca9d40606  (my api key)
+ // 89c4250809c202e3e1990eca9d40606  (my api key)
