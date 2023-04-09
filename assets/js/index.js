956657e3c0ada1f364ca484ea165f52a7f2ac603
Amanda Gray <floridabelle2021@protonmail.com>
2023-04-08 20:07:56 -0600

    creating the info cards

--- assets/js/index.js
+++ assets/js/index.js
@@ -61,9 +61,9 @@
      $(cardTodayBody).empty;
 
      $.ajax({
-         url: getUrlCurrent,
-         method: "GET",
-     }).then(function (response) {
+             url: getUrlCurrent,
+             method: "GET",
+         }).then(function (response) {
              $('.cardTodayCityName').text(response.name);
              $('.cardTodayDate').text(date);
              $('.icons').attr('src', `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`);
@@ -86,77 +86,88 @@
                  url: getUrlUvi,
                  method: 'GET',
              }).then(function (response) {
-                     var pElUvi = $('<p>').text(`UV Index; `);
-                     var uviSpan = $('<span>').text(response.current.uvi);
-                     var uvi = reponse.current.uvi;
-                     pElUvi.append(uviSpan);
-                     cardTodayBody.append(pEluvi);
+                 var pElUvi = $('<p>').text(`UV Index; `);
+                 var uviSpan = $('<span>').text(response.current.uvi);
+                 var uvi = reponse.current.uvi;
+                 pElUvi.append(uviSpan);
+                 cardTodayBody.append(pEluvi);
 
-                     if (uvi >= 0 && uvi <= 2) {
-                         uviSpan.attr('class', 'green');
-                     } else if (uvi > 2 && uvi <= 5) {
-                         uviSpan.attr('class', "yellow");
-                     } else if (uvi > 5 && uvi <= 7) {
-                         uviSpan.attr("class", "red")
-                     } else {
-                         uviSpan.attr("class", "purple")
-                     }
-                    //  created and if/else if for the UVI and colors to display the extremeness
-                 });
-             }):
+                 if (uvi >= 0 && uvi <= 2) {
+                     uviSpan.attr('class', 'green');
+                 } else if (uvi > 2 && uvi <= 5) {
+                     uviSpan.attr('class', "yellow");
+                 } else if (uvi > 5 && uvi <= 7) {
+                     uviSpan.attr("class", "red")
+                 } else {
+                     uviSpan.attr("class", "purple")
+                 }
+                 //  created and if/else if for the UVI and colors to display the extremeness
+             });
+         }):
+
+         getFiveDayForecast();
+ };
+
+ var fiveForecastEl = $('.fiveForecast');
+
+ function getFiveDayForecast() {
+     var getUrlFiveDay = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${key}`;
+
+     $.ajax({
+         url: getUrlFiveDay,
+         method: "GET",
+     }).then(function (response) {
+         var fiveDayArray = reponse.list;
+         var myWeather = [];
+
+         $.each(fiveDayArray, function (index, value) {
+             testObj = {
+                 date: value.dt_txt.split(' ')[0],
+                 time: value.dt_txt.split(' ')[1],
+                 temp: value.main.temp,
+                 feels_like: value.main.feels_like,
+                 icon: value.weather[0].icon,
+                 humidity: value.main.humidity
+             }
+             if (value.dt_text.split('')[1] === "12:00:00") {
+                 myWeather.push(testObj);
+             }
+         })
+         // above is an object array for easy reading in console
 
 
+         //  creating the info cards
+         for (let i = 0; i < myWeather.length; i++) {
 
+             var divElCard = $('<div>');
+             divElCard.attr("class", "card test-white bg-primary mb-3 cardOne");
+             divElCard.attr("style", 'max-width: 200px');
+             fiveForecastEl.append(divElCard);
 
+             var divElHeader = $('<div>');
+             divElHeader.attr('class', 'card-header')
+             var m = moment(`${myWeather[i].date}`).format('MM-DD-YYYY');
+             divElHeader.text(m);
+             divElCard.append(divElHeader);
 
+             var divElBody = $('<div>');
+             divElBody.attr('class', 'card-body');
+             divElCard.append(divElBody);
 
-     })
+             var divElIcon = $('<img>');
+             divElIcon.attr('class', 'icons');
+             divElIcon.attr('src', `https://openweathermap.org/img/wn/${myWeather[i].icon}@2x.png`);
+             divElBody.append(divElIcon);
 
- $.ajax({
-     url: getUrlFiveDay,
-     method: "GET",
- }).then(function (response) {
-     var fiveDayArray = reponse.list;
-     var myWeather = [];
-
-     $.each(fiveDayArray, function (index, value) {
-         testObj = {
-             date: value.dt_txt.split(' ')[0],
-             time: value.dt_txt.split(' ')[1],
-             temp: value.main.temp,
-             feels_like: value.main.feels_like,
-             icon: value.weather[0].icon,
-             humidity: value.main.humidity
+             var pElTemp = $('<p>').text(`Temperature: ${myWeather[i].temp} °F`);
+             divElBody.append(pElTemp);
+             var pElFeel = $('<p>').text(`Feels Loke: ${myWeather[i].feels_like} °F`);
+             divElBody.append(pElFeel);
+             var pElHumid = $('<p>').text(`Humidity: ${myWeather[i].humidity} %`);
+             divElBody.append(pElHumid);
          }
-         if (value.dt_text.split('')[1] === "12:00:00") {
-             myWeather.push(testObj);
-         }
-     })
-     // above is an object array for easy reading in console
-
-
-     //  creating the info cards
-     for (let i = 0; i < myWeather.length; i++) {
-
-         var divElCard = $('<div>');
-         divElCard.attr("class", "card test-white bg-primary mb-3 cardOne");
-         divElCard.attr("style", 'max-width: 200px');
-
-
-
-
-     }
-
-
-
-
-
- })
-
-
-
- }
-
+     });
+ };
 
 
 
